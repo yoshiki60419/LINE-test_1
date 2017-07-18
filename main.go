4c08c05e286d5745136105cd05ffe729c493f51c
Harvey <yoshiki60419@hotmail.com>
2017-07-18 17:53:15 +0800

    test

--- main.go
+++ main.go
@@ -27,13 +27,13 @@ func main() {
 	var err error
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
-	http.HandleFunc("/callback", callbackHandler)
+	http.HandleFunc("/callback", callbackHandler01)
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
 	http.ListenAndServe(addr, nil)
 }
 
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
+func callbackHandler01(w http.ResponseWriter, r *http.Request) {
 	events, err := bot.ParseRequest(r)
 
 	if err != nil {
@@ -53,6 +53,30 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Print(err)
 				}
 			}
+		}
+	}
+}
+
+func callbackHandler02(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			
+			case *linebot.TextMessage:
+				if _, err = bot.PushMessage(<to>, linebot.NewTextMessage(" 洋蔥姐該起床囉~")).Do(); err != nil {
+					log.Print(err)
+				}
+			
 		}
 	}
 }
