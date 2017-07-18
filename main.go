ccd924d0e76419f9ef13f95be93c1b6518469d02
Harvey <yoshiki60419@hotmail.com>
2017-07-18 14:47:36 +0800

    修改訊息
    修改回應

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+" woo~ya~")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+" 洋蔥姐很會ㄛ~")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
