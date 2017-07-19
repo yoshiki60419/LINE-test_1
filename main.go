f606ebaebb872bca2360c6e2ea5594dee392d2ab
Harvey <yoshiki60419@hotmail.com>
2017-07-19 09:43:29 +0800

    test

--- main.go
+++ main.go
@@ -48,11 +48,11 @@ func callbackHandler01(w http.ResponseWriter, r *http.Request) {
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
+			/*case *linebot.TextMessage:
+			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+" 洋蔥姐很會ㄛㄛ~")).Do(); err != nil {
+				log.Print(err)
+			}*/
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+" 洋蔥姐很會ㄛㄛ~")).Do(); err != nil {
-					log.Print(err)
-				}
-			case *linebot.ImageMessage:
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+" 嗚耶哥最會ㄨㄝ~")).Do(); err != nil {
 					log.Print(err)
 				}
